a526913ef5ade96649e61957cc1025810a3aeff8
helenahwang <helenayhwang@gmail.com>
2018-07-06 12:33:46 +0900

    Upload

---
+++ 0706Class/src/example/Main.java
@@ -0,0 +1,15 @@
+package example;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("이 문장은 실행 됨");
+		System.exit(0);// 0 : 정상 종료  1: 1번문제가 생겨서 끝낸다. 그래서 나머지는 너가 해결해줘...
+		
+		System.out.println("이 문장은 실행 안됨");
+
+	}
+
+}
