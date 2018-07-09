987b60c71a9e781d24e339ab9049512fea20603a
503-02 <503-02@DESKTOP-65LMUA3>
2018-07-09 11:48:43 +0900

    Upload

--- 0706Class/src/problem/Main.java
+++ 0706Class/src/problem/Main.java
@@ -5,27 +5,30 @@ public class Main {
 	public static void main(String[] args) {
 		
 		
-		int[] ar= {10,30,70,40,15};
-		/*
+		int[] ar= {10,30,20,70,10,40,15};
+		
 		//가장 큰 수의 위치
 		
-		
+		/*
 		int mmax=0;
+		int max=0;
 		for(int i=0; i<ar.length; i++) {
 			
-			for(int j=i; j<ar.length; j++) {
+			for(int j=i+1; j<ar.length; j++) {
 				
-				if(ar[i]>ar[j]) {
-					mmax=i;
+				if(mmax < ar[j]) {
+					mmax=ar[j];
+					max=j;
 					
 				}
 				
 			}
 		}
 		
-		System.out.println("가장 큰 값: "+ar[mmax]);
+		System.out.println("가장 큰 값: "+ar[max]+" 인덱스 값: "+max);
 		*/
 		
+		/*
 		int max=ar[0];
 		int idx=0;
 		int size=ar.length;
@@ -39,18 +42,33 @@ public class Main {
 		}
 		System.out.println("가장 큰수 위치: "+idx);
 		
-		
-		
-		
-		
-		
+		*/
 		
 		
 		
 		
 		//50에 가장 가까운 수의 위치
 		
+		double[] dif = new double[ar.length]; 
 		
+		for(int i=0; i<ar.length; i++) {
+			int diff = Math.abs(ar[i]-50);
+			dif[i]=diff;
+		}
+		
+		double dd=dif[0];
+		int min=0;
+		for(int i=1; i<ar.length; i++) {
+			
+			if(dd>dif[i]) {
+				
+				dd=dif[i];
+				min=i;
+				
+			}
+		}
+		
+		System.out.println("50에 가장 가까운 숫자 : "+ar[min] +"  위치는 : "+min);
 
 	}
 
