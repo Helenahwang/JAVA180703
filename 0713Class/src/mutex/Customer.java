f947cf7485f428998e25d4eeb233e7f2938f75f7
503-02 <503-02@DESKTOP-65LMUA3>
2018-07-16 18:08:46 +0900

    Upload

--- 0713Class/src/mutex/Customer.java
+++ 0713Class/src/mutex/Customer.java
@@ -1,40 +1,18 @@
 package mutex;
 
-import java.util.ArrayList;
 
 //List에서 데이터를 26번 꺼내서 출력하는 스레드
 public class Customer extends Thread {
-	private ArrayList<Character> list2;
+	private ShareData shareData;
 	
-	public Customer(ArrayList<Character> list2) {
-		this.list2=list2;
+	public Customer(ShareData shareData) {
+		this.shareData=shareData;
 	}
 	
-	//list에서 26개를 꺼내서 출력하는 메소드
-	public synchronized void get() {
-		//for(int i=0; i<26; i=i+1) {
-			//list에 데이터가 없으면 대기
-			if(list2.size()==0) {
-				try {
-					wait();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-			
-			char ch=list2.remove(0); //0번째 것을 항상 지운다.
-			System.out.println(ch+"를 소비했습니다.");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-	//}
+
 	public void run() {
 		for(int i=0; i<26; i++) {
-			get();
+			shareData.get();
 		}
 	}
 
--- Hancom/src/problem/Main2.java
+++ Hancom/src/problem/Main2.java
@@ -6,126 +6,63 @@ public class Main2 {
 
 	public static void main(String[] args) {
 		
-		String acgtSequence="ACCGGCCG";
-		//String acgtSequence="ACCGGCCGAGACAGCGAGCATATGCCGCCGAGACAGGCCGGACCGGCCGAGA";
-		String condon="GCCG";
-		
-		int index=0;
-		int count=0;
-		
-		LinkedList<Integer> ar = new LinkedList<>();
-		char ac=' ';
-		char co=' ';		
-		
-		while(true) {
+		 
+		  //String acgtSequence="ACCGGCCG";
+		  String acgtSequence="ACCGGCCGAGACAGCGAGCATATGCCGCCGAGACAGGCCGGACCGGCCGAGA";
+		  String condon="GCCG";
+		  
+		  int index=0;
+		  int count=0;
+		  
+		  LinkedList<Integer> ar = new LinkedList<>();
+		  char ac=' ';
+		  char co=' ';  
+		  
+		  while(true) {
+		   
+		   if(condon.length()>acgtSequence.length()-index) {
+		    break;
+		   } 
+		   
+		   //System.out.println(index);
+		   count=index;
+		   
+		   for(int i=0; i<condon.length(); i++) {
 
-			//System.out.println(index);
-			//System.out.println(acgtSequence.length());
-			
-			if(index>acgtSequence.length()-condon.length()) {
-				break;
-			}	
-			
-			count=index;
-			
-			for(int i=0; i<condon.length(); i++) {
+		    ac=acgtSequence.charAt(count);
+		    co=condon.charAt(i);
+		         
+		    if(i==condon.length()-1) {
+		     if(ac==co) {
+		      
+		      //System.out.println("index : "+index);
+		      //System.out.println("count : "+count);
+		      ar.add(index);
+		      index=count;
+		      
+		     }
+		     index++;
+		    }
+		    
+		    if(ac==co) {
+		     
+		     count++;
+		     
+		     
+		    }else { 
+		     
+		     
+		     index++;
+		     break;
+		    }
+		   }
+		   
+		   
+		   
+		  }
+		  
+		  System.out.println(ar);
 
-				ac=acgtSequence.charAt(count);
-				co=condon.charAt(i);
+		 }
 
-				System.out.println(ac + "  "+co);
-				
-				if(ac==co) {
-					
-					count++;
-					
-					if(i==condon.length()-1) {
-
-						ar.add(index);
-						System.out.println(ar);
-						break;
-					}
-
-				}
-				else {	
-					
-					
-					index++;
-					break;
-				}
-			}
-			
-
-
-
-
-
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-		/*	
-			while(true) {
-			
-			int count=0;
-			char ac=acgtSequence.charAt(index);
-			char co=condon.charAt(count);
-			int[] ar = null;
-			
-			System.out.println(1+" index = "+index);
-			
-			if(ac==co) {
-				index++;
-				count++;
-				ac=acgtSequence.charAt(index);
-				co=condon.charAt(count);
-				System.out.println(2+" index = "+index);
-				
-				if(ac==co) {
-					index++;
-					count++;
-					ac=acgtSequence.charAt(index);
-					co=condon.charAt(count);
-					System.out.println(3+" index = "+index);
-					
-					if(ac==co) {
-						index++;
-						count++;
-						ac=acgtSequence.charAt(index);
-						co=condon.charAt(count);
-						System.out.println(4+" index = "+index);
-						
-						if(ac==co) {
-							index++;
-							ac=acgtSequence.charAt(index);
-							co=condon.charAt(count);
-							System.out.println(5+" index = "+index);
-							ar[cc]=index;
-							cc++;
-							
-						}
-					}
-				}
-			}else {
-				index++;
-			}
-			
-			}
-			*/
-			
-			
-			
-		}
-
-	}
-
-}
+		} 
