ffc5e7a02f74afa5daa9d3b4b16cf3b469ee92b4
503-02 <503-02@DESKTOP-65LMUA3>
2018-07-18 09:19:40 +0900

    Upload

--- Hancom/src/problem/Main3.java
+++ Hancom/src/problem/Main3.java
@@ -6,79 +6,82 @@ public class Main3 {
 
 	public static void main(String[] args) {
 
-		HashMap<Integer,Integer> ar=new HashMap<>();
+		  HashMap<Integer,Integer> ar=new HashMap<>();
+		  HashMap<Integer,Integer> sign=new HashMap<>();
+		  int sum=0;
+		  int check=0;
 
-		HashMap<Integer,Integer> sign=new HashMap<>();
-		int sum=0;
-		int check=0;
-		int num=1;
-		int po=num;
-	
-		
-		
-		int target=12;
-		
-		
-		int count=0;
-		
-
-		while(count<10) {
-
-			
-			for(int j=1; j<=num; j++) {
-				sign.put(j, 1);
-			}
-
-			count=0;
-			
-			while(count < Math.pow(2, num)) {
+		  int num=1;
+		  int target=12;//-3646397;
 
 
-				for(int i=1; i<=num; i++) {
-
-					if((count%(Math.pow(2,po)/Math.pow(2,i))==0) && count!=0) {
-
-						if(sign.get(i)>0) {
-
-							sign.put(i,-1);
-						}
-						else {
-
-							sign.put(i,1);
-						}
+		  int count=0;
 
 
-					}
+		  while(true) {
 
 
-					int value=i*sign.get(i);
-					sum=sum+value;
+		   if(check==1) {
+		    break;
+		   }
 
-				}
-				ar.put(count, sum);
 
-				sum=0;
-				
-				System.out.println(count+ "  "+ar.get(count));
-			
-				if(ar.get(count)==target) { 
-					
-					check=1;
-					break;			
-				}
-				
-				
-				count++;
-				
-			}
+		   sign.clear();
+		   ar.clear();
 
-			num++;
-		
-			if(check==1) {
-				break;
-			}
+		   for(int j=1; j<=num; j++) {
+		    sign.put(j, 1);
 
-		}
-	}
+		   }
 
-} 
+		   count=0;
+
+		   while(count < Math.pow(2, num)) {
+
+		    for(int i=1; i<=num; i++) {
+
+		     if((count%(Math.pow(2,num)/Math.pow(2,i))==0) && count!=0) {
+
+
+		      if(sign.get(i)>0) {
+
+		       sign.put(i,-1);
+		      }
+		      else {
+
+		       sign.put(i,1);
+		      }
+
+		     }
+
+
+		     int value=i*sign.get(i);
+		     sum=sum+value;
+
+		    }
+		    //System.out.println(sum);
+		    ar.put(count, sum);
+
+		    sum=0;
+
+		    if(ar.get(count)==target) { 
+
+		     check=1;
+		     System.out.println(num);
+		     break;   
+		    }
+
+		    count++;
+
+		   }
+
+		   //System.out.println(num);
+		   num++;
+		   
+
+		  }
+		  
+
+		 }
+
+		}  
