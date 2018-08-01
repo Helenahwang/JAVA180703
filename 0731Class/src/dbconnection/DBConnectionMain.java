0690b983c0493495f363d4edbb67d683f41126f2
503-02 <503-02@DESKTOP-65LMUA3>
2018-08-01 12:25:45 +0900

    Upload

--- 0731Class/src/dbconnection/DBConnectionMain.java
+++ 0731Class/src/dbconnection/DBConnectionMain.java
@@ -7,14 +7,20 @@ import java.sql.DriverManager;
 public class DBConnectionMain {
 
 	public static void main(String[] args) {
-		//사용하고자 하는 드라이버 클래스를 로드
-		//데이터베이스 종류에 따라 다르다
+		
 		
 		//데이터베이스 연결 변수 선언
 		//반드시 close()를 호출해야 하므로 finally에서 close()를 호출
 		Connection con=null;
 		try {
+			
+			//사용하고자 하는 드라이버 클래스는 로드 데이터베이스 종류에 따라 다르다
 			Class.forName("oracle.jdbc.driver.OracleDriver");
+			
+			//확인하기 위한 코드, 이 코드가 출력되지 않으면 드라이버 이름을 확인해보고 드라이버이름이 틀리지 않았다면 ojdbc6.jar가 Referenced Libraries에 포함되어 있는지 확인
+			System.out.println("오라클 드라이버 로드 성공");
+			
+			//데이터베이스 연결
 			//con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","scott","tiger");
 			con=DriverManager.getConnection("jdbc:oracle:thin:@192.168.0.234:1521:xe","scott","tiger");
 			System.out.println(con);
